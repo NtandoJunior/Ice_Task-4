d155fe83b8f9d04b2b37184f1f70f0886e10e23d
NtandoJunior <ntandojunior020@gmail.com>
2025-08-13 12:55:22 +0000

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package demo.test;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        
+       String [] places = {"JHB", "DBN", "CTN", "PE"};
+       String [][] speed = {{"128km", "135km", "139km"}, {"155km", "129km", "175km"}, {"129km", "130km", "185km"}, {"195km", "155km", "221km"} };
+       String [] months = {"JAN", "FEB", "MAR"};
+       }
+    }
+
+ for (String [] col : places) {
+            for (String i : col) {
+                System.out.print(i),
+            }
+for (String [][] row : speed) {
+            for (String i : row) {
+                System.out.print(i + "\t"),
+            }
+for (String [] row : months) {
+            for (String i : row) {
+                System.out.print(i),
+            }
+
+        
+
+
+System.out.println("***************")
+System.out.println("SPEEDING FINES REPORT")
+System.out.println("***************")
+    
+
+System.out.println("***************")
+System.out.println("SPEEDING FINES STATISTICS")
+System.out.println("***************")
+    
+
+System.out.println("Maximum speed captured=" + "221km" )
+System.out.println("Minimum speed captured=" + "128km")
+}
+System.out.print();
+
+
